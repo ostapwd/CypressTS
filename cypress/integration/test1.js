37123cfcfe094ed153f417995b695f86f6b705b7
Ostap Voitsekhovskyi <ostap_voitsekhovskyi@epam.com>
2022-05-03 20:26:50 +0300

    Mocha intro

--- cypress/integration/test1.js
+++ cypress/integration/test1.js
@@ -1,6 +1,6 @@
 describe('Test suite', function () {
 
-    it.only('Test 1', function () {
+    it('Test 1', function () {
 
         cy.visit("https://www.saucedemo.com/");
 
@@ -39,10 +39,90 @@ describe('Test suite', function () {
 
     });
 
-    it.skip('Test 1', function () {
+    it('Test 1', function () {
 
         cy.visit("https://www.google.com/");
 
         cy.get("[name='q']").type("what is Cypress").blur();
+    });
+});
+
+describe.only('Test suite for login', function () {
+
+    before(function() {
+        cy.log("Main before");
+    });
+
+    beforeEach(function() {
+        cy.log("main before each");
+
+        cy.visit("https://www.saucedemo.com/");
+    });
+
+    describe('Test suite for login (positive)', function () {
+
+        before(function() {
+            cy.log("Internal before");
+        });
+
+        beforeEach(function() {
+            cy.log("before each");
+        });
+
+        it('Test 1 (positive login)', function () {
+            cy.get("#user-name").type("standard_user");
+            cy.get("#password").type("secret_sauce");
+
+            cy.get("#login-button").click();
+
+            cy.get("span.title").should("contain", "Products");
+        });
+
+        it('Test 2 (positive login)', function () {
+            cy.get("#user-name").type("standard_user");
+            cy.get("#password").type("secret_sauce");
+
+            cy.get("#login-button").click();
+
+            cy.get("span.title").should("contain", "Products");
+        });
+
+        afterEach(function() {
+            cy.log("after each");
+        });
+
+        after(function() {
+            cy.log("Internal after");
+        });
+
+    });
+
+
+    describe('Test suite for login (negative)', function () {
+
+        beforeEach(function() {
+            cy.log("before each");
+
+            cy.get("#user-name").type("standard_user");
+        });
+
+        it('Test 3 (negative login)', function () {
+            cy.get("#password").type("secret_sauce1111");
+
+            cy.get("#login-button").click();
+
+            cy.get("h3[data-test='error']").should("contain", "Epic sadface: Username and password do not match any user in this service");
+        });
+
+        it('Test 4 (negative login)', function () {
+            cy.get("#login-button").click();
+
+            cy.get("h3[data-test='error']").should("contain", "Epic sadface: Password is required");
+        });
+
+    });
+
+    after(function() {
+        cy.log("Main after");
     });
 });
