d2647e46f785c674789007b55f8d2d7c633fc329
MariiaYaskiv <lytvyn.mariia@gmail.com>
2022-05-05 16:21:00 +0300

    Added some changes with css and xpath locators to  the first-test. Also added new file mocha-test-practice. There was added some code with mocha

--- cypress/integration/first-test.js
+++ cypress/integration/first-test.js
@@ -4,8 +4,8 @@ describe ("First test", function () {
       cy.visit("https://www.saucedemo.com/");
 
       cy.xpath('//*[@id="user-name"]').type('standard_user');
-      cy.get("#password").type("secret_sauce");
-      cy.get("#login-button");
+      cy.get("input[name='password']").type("secret_sauce");
+      cy.get("input[data-test='login-button']");
       cy.contains('Login')
       .click();
 
@@ -18,7 +18,7 @@ describe ("First test", function () {
 
        cy.visit("https://www.saucedemo.com/");
 
-       cy.get("#user-name").type("standard_user");
+       cy.xpath('//*[@id="user-name"]').type("standard_user");
        cy.xpath("//input[@placeholder='Password']").type("secret_sauce123");
        cy.xpath("//input[contains(@id, 'login-button')]");
        cy.contains('Login')
@@ -32,38 +32,38 @@ describe ("First test", function () {
 
        cy.visit("https://www.saucedemo.com/");
 
-       cy.get("#user-name").type("standard_user");
+       cy.xpath('//*[@id="user-name"]').type("standard_user");
        cy.xpath("//input[@placeholder='Password']").type("secret_sauce");
-       cy.get("#login-button");
+       cy.get("input[data-test='login-button']");
        cy.contains('Login')
        .click()
 
        cy.get("#add-to-cart-sauce-labs-backpack").click();
        cy.xpath("//div[@id='shopping_cart_container']").click();
-       cy.get(".bm-burger-button").click();
+       cy.xpath("//div[@class='bm-burger-button']").click();
        cy.contains("All Items").click();
 
 
        cy.xpath("//button[@id='remove-sauce-labs-backpack']").click();
 
-       cy.get("#shopping_cart_container").click();
+       cy.get("div .shopping_cart_link").click();
 
     });
     it('Adding/deleting the item to/from the "Shopping cart - Another way"', function () {
 
        cy.visit("https://www.saucedemo.com/");
 
-       cy.get("#user-name").type("standard_user");
-       cy.get("#password").type("secret_sauce");
-       cy.get("#login-button");
+       cy.xpath('//*[@id="user-name"]').type("standard_user");
+       cy.get("input[name='password']").type("secret_sauce");
+       cy.get("input[data-test='login-button']");
        cy.contains('Login')
        .click();
 
        cy.get("#add-to-cart-sauce-labs-backpack").click();
        cy.get(".shopping_cart_badge").click();
-       cy.get('.header_secondary_container').then( label => {
+       cy.xpath("//div[@class='header_secondary_container']").then( label => {
        expect(label.text()).to.equal('Your Cart');
-       cy.get("#remove-sauce-labs-backpack").click();
+       cy.get("button[name='remove-sauce-labs-backpack']").click();
        cy.xpath("//button[@id='continue-shopping']").click();
     });
 });
