d6a9f7706f7f9362f94e5d68f6bee616e47384f7
Natalia_Paraniak <92261072+NataParaniak@users.noreply.github.com>
2022-05-08 17:35:39 +0300

    nata test

--- cypress/integration/test1.js
+++ cypress/integration/test1.js
@@ -1,48 +1,9 @@
-describe('Test suite', function () {
+describe(" Log in", function(){
+    it ("Sing in", function(){
+        cy.visit("https://www.saucedemo.com/")
+        cy.get("#user-name").type("standard_user")
+        cy.get("#password").type("secret_sauce")
+        cy.get("#login-button").click()
 
-    it.only('Test 1', function () {
-
-        cy.visit("https://www.saucedemo.com/");
-
-        cy.wait(2000);
-
-        //cy.xpath("//form/div").click({ multiple: true });
-
-        cy.wait(2000);
-        cy.get("#user-name" ,{timeout:25000}).type("standard_user").click();
-        cy.get("#password").type("secret_sauce");
-
-        cy.get("#login-button").click();
-
-        cy.get("#header_container .title").then(element =>
-            cy.log(element.text())
-        );
-
-        cy.get("#header_container .title").should('contain', "Product");
-
-        cy.wait(3000);
-        cy.contains("Add to cart").click();
-
-        cy.wait(2000);
-
-        cy.get(".inventory_item_name").each(item => {
-            cy.log(item.text())
-        });
-
-        cy.get("[id*='add-to-cart']").each(item => {
-            item.click();
-        });
-
-        cy.xpath("//*[@id='react-burger-menu-btn']").click();
-
-
-
-    });
-
-    it.skip('Test 1', function () {
-
-        cy.visit("https://www.google.com/");
-
-        cy.get("[name='q']").type("what is Cypress").blur();
-    });
-});
+    })
+})
