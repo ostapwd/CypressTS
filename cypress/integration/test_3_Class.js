3869b86954b3fcd23f7e609830869b30aa0e67f6
VitalikTR <vitaliktr@gmail.com>
2022-05-14 11:44:07 +0300

    deleted commented text

--- cypress/integration/test_3_Class.js
+++ cypress/integration/test_3_Class.js
@@ -1,7 +1,3 @@
-// import users from "../data/users";
-// import LoginPageTS from "../support/pages/loginPage";
-// import ProductPageTS from "../support/pages/productPage";
-
 import users from "../data/users";
 import loginPagePractice from "../support/pages/loginPagePractice";
 import productPagePractice from "../support/pages/productPagePractice";
@@ -29,12 +25,3 @@ describe('Test suite', () => {
         loginPagePractice.loginPageLogoLabel.should("be.visible")
     });
 });
-
-// describe('Test suite', function () {
-//     it('Verify a user can add all products to the cart', () => {
-//         loginPage.open()
-//             .loginToTheApp(users.standardUser)
-//             .addToCartAllproducts()
-//             .verifyThatNumberSelectedProductsEquals("6");
-//     });
-// });
