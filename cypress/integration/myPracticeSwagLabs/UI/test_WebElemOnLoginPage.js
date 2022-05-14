cc098f65e416086cf7d2ea404af1445824b8513f
VitalikTR <vitaliktr@gmail.com>
2022-05-14 23:47:41 +0300

    added folder myPracticeSwagLabs

---
+++ cypress/integration/myPracticeSwagLabs/UI/test_WebElemOnLoginPage.js
@@ -0,0 +1,24 @@
+
+describe('Test suite to test web elements on the login page', () => {
+   
+    beforeEach(() => {
+        
+    });
+
+       // it('Verify a user can login to the aplication', () => {
+    //     productPagePractice.productsLabel.then((element) => {
+    //         expect(element.text()).to.equal('Products')
+    //     })
+    // });
+
+    // it('Verify a user can add all products to the cart', () => {
+    //     productPagePractice.addToCartAllproducts().selectedProductsNumber.then((element) => {
+    //         expect(element.text()).to.equal('6')
+    //     })
+    // });
+
+    // it('Verify a user can logout form the app', () => {
+    //     productPagePractice.openMenu().waitTime().logoutApp();
+    //     loginPagePractice.loginPageLogoLabel.should("be.visible")
+    // });
+});
