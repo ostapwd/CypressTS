0b1eb50afd440d73542e55995d89eb72c8fcbe1d
MariiaYaskiv <lytvyn.mariia@gmail.com>
2022-05-17 10:48:03 +0300

    added users page to testing web-site Parabank

---
+++ cypress/data/usersParabank.ts
@@ -0,0 +1,47 @@
+class UsersParabank {
+
+    standardUser = {
+        firstName: 'Mariia',
+        lastName: 'Mariia',
+        addressStreet: 'Mariia',
+        addressCity: 'Mariia',
+        addressState: 'Mariia',
+        addressZipCode: 'Mariia',
+        phoneNumber: 'Mariia',
+        ssn: 'Mariia',
+        username: 'Mariia',
+        password: 'Mariia',
+        repeatedPassword: 'Mariia'
+    };
+
+    login = {
+        username: 'Mariia',
+        password: 'Mariia'
+    };
+
+    forgotInformation = {
+        firstName: 'Nastya',
+        lastName: 'Mariia',
+        addressStreet: 'Mariia',
+        addressCity: 'Mariia',
+        addressState: 'Mariia',
+        addressZipCode: 'Mariia',
+        ssn: 'Nastya'
+       
+       
+    };
+    forgotAllInformation = {
+        firstName: ' ',
+        lastName: ' ',
+        addressStreet: ' ',
+        addressCity: ' ',
+        addressState: ' ',
+        addressZipCode: ' ',
+        ssn: 'Nastya'
+    };
+ 
+
+}
+
+export default new UsersParabank();
+
