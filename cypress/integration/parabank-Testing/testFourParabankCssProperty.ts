54eced72b541d35b182546385a924e94d97e0fba
MariiaYaskiv <lytvyn.mariia@gmail.com>
2022-05-18 17:31:33 +0300

    added a new tests to check the property of backgraund of element in left menu

---
+++ cypress/integration/parabank-Testing/testFourParabankCssProperty.ts
@@ -0,0 +1,45 @@
+
+import usersParabank from "../../data/usersParabank";
+import {MainParabankPageLoginRegister} from "../../support/pages/parabank-pages/parabankLoginPage";
+import { MainParabankPageLeftMenu } from "../../support/pages/parabank-pages/mainParabankPageLeftMenu";
+
+const l = new MainParabankPageLoginRegister();
+const m = new MainParabankPageLeftMenu();
+
+describe ("Third test suite for Parabank", function () {
+    beforeEach('login to the app', () => {
+        l.open()
+    })
+
+    it("Test 1 Checking css property 'Solutions' link (positive)", () => {
+        m.leftMenuSolutionsElementCheckingVrongCssProperty() 
+    })
+
+    it("Test 2 Checking css property 'Solutions' link (negative)", () => {
+        m.leftMenuSolutionsElementCheckingCssProperty() 
+    })
+
+    it("Test 3 Checking css property 'About Us' link ", () => {
+        m.leftMenuAboutUsElementCheckingCssProperty()
+    })
+
+    it("Test 4 Checking css property 'Services' link ", () => {
+        m.leftMenuServicesElementCheckingCssProperty()
+    })
+
+    it("Test 5 Checking css property 'Products' link ", () => {
+        m.leftMenuProductsElementCheckingCssProperty()
+    })
+
+    it("Test 6 Checking css property 'Locations' link ", () => {
+        m.leftMenuLocationsElementCheckingCssProperty()
+    })
+
+    it("Test 7 Checking css property 'Services' link ", () => {
+        m.leftMenuAdminsElementCheckingCssProperty()
+    })
+    afterEach(function () {
+        cy.log("after Each");
+    });
+    
+})
