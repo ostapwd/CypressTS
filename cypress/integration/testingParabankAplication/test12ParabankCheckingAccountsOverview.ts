a6b513303b2c452ba81a44fb8a8eeec24ead81d8
MariiaYaskiv <lytvyn.mariia@gmail.com>
2022-05-24 17:43:38 +0300

    added new test to test12ParabankCheckingAccountsOverview (it is not finished)

---
+++ cypress/integration/testingParabankAplication/test12ParabankCheckingAccountsOverview.ts
@@ -0,0 +1,34 @@
+import {MainParabankPageAccountOverview} from "../../support/pages/parabankPages/mainParabankPageAccountsOverview"
+import usersParabank from "../../data/usersParabank";
+import {MainParabankPageLoginRegister} from "../../support/pages/parabankPages/mainParabankPageLoginRegister";
+
+const l = new MainParabankPageLoginRegister();
+const na = new MainParabankPageAccountOverview();
+
+describe ("Test for Parabank, Accounts Overview", function () {
+    beforeEach('login to the app', () => {
+        l.open()
+        .registerToTheApp(usersParabank.thirdUser)
+        .buttonRegister();
+    });
+
+    it("Test 1 to Accounts Overview in Parabank", () => {
+
+
+    na.clickLinkAccountOverview()
+    .weMustCheckIfWeAreInTheCorrectPage()
+    .enterToAccountToSeeMoreDetails()
+    .chooseMonthAccountOverview('All')
+    .chooseTransactionTypeClick('All')
+    .buttonGoFromAccountActivity()
+    na.checkingIfThereAreEnoughtInputsInTransactionsTable()
+    //.checkingThatTheFillsTransactionsTableAreNotEmpty()
+
+})
+
+afterEach(function () {
+    cy.log("after Each");
+});
+
+
+})
