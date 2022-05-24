af8f48b4677a0e1632b54a1d62d55a2f0cbfc841
MariiaYaskiv <lytvyn.mariia@gmail.com>
2022-05-24 11:01:30 +0300

    added tests with CustomerCare (Test5Parabank)

---
+++ cypress/integration/testingParabankAplication/test5ParabankCheckingTheerdButtonTopLeftItemsCustomenCare.ts
@@ -0,0 +1,33 @@
+import contactUsers from "../../data/contactUsers";
+import {MainParabankPageLoginRegister} from "../../support/pages/parabankPages/mainParabankPageLoginRegister";
+import { MainParabankPageLeftMenu } from "../../support/pages/parabankPages/mainParabankPageHeaderPanelTopLeftMenu";
+import {MainParabankPageContact} from "../../support/pages/parabankPages/mainParabankPageHeaderPanelContact"
+
+
+const l = new MainParabankPageLoginRegister();
+const m = new MainParabankPageLeftMenu();
+const c = new MainParabankPageContact();
+
+
+describe ("Fifth test suite for Parabank", function () {
+    beforeEach('login to the app', () => {
+        l.open()
+    })
+
+    it("Test 1 Send message to the customer care with all requirements", () => {
+    c.customerCare(contactUsers.standardUser)
+    })
+
+    it("Test 1 Send message to the customer care without all requirements", () => {
+        c.forgotInputAllFillsCustomerCare(contactUsers.forgotUser)
+        })
+
+
+        it("Test 1 Check data entry requirements if all inputs are empty", () => {
+            c.forgotInputAllFillsCustomerCare(contactUsers.forgotUser)
+            .responseInTheTableWhenTheAllInputsAreNotFilledInCustomerCare() 
+
+            })
+
+
+})
