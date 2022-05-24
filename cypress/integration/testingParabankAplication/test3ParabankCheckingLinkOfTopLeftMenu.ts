bafd0351cfa960bb290ac7cc0f775ba93bc6c060
MariiaYaskiv <lytvyn.mariia@gmail.com>
2022-05-24 11:21:46 +0300

    remained test3Parabank and changed the folder

---
+++ cypress/integration/testingParabankAplication/test3ParabankCheckingLinkOfTopLeftMenu.ts
@@ -0,0 +1,40 @@
+import usersParabank from "../../data/usersParabank";
+import {MainParabankPageLoginRegister} from "../../support/pages/parabankPages/mainParabankPageLoginRegister";
+import { MainParabankPageLeftMenu } from "../../support/pages/parabankPages/mainParabankPageHeaderPanelTopLeftMenu";
+
+const l = new MainParabankPageLoginRegister();
+const m = new MainParabankPageLeftMenu();
+
+describe ("Third test suite for Parabank", function () {
+    beforeEach('login to the app', () => {
+        l.open()
+    })
+
+    it("Test 1 Checking the link 'Solutions' if it go to to the correct link ", () => {
+       m.leftMenuElementSolutionGoToAnotherPageClick()
+    });
+
+    it("Test 2 Checking the link 'About Us' if it go to to the correct link ", () => {
+        m.leftMenuEboutUsGoToAnotherPageClick()
+     });
+    
+     it("Test 3 Checking the link 'Services' if it go to to the correct link ", () => {
+        m.leftMenuServicesGoToAnotherPageClick()
+     });
+
+     it("Test 4 Checking the link 'Admin' if it go to to the correct link ", () => {
+        m.leftMenuAdminGoToAnotherPageClick() 
+     });
+
+     it("Test 5 Checking the link 'Products' if it go to to the correct link ", () => {
+        m.leftMenuProductsGoToAnotherPageClick()
+     });
+
+     it("Test 6 Checking the link 'Location' if it go to to the correct link ", () => {
+        m.leftMenuLocationsGoToAnotherPageClick()
+     });
+     
+     afterEach(function () {
+      cy.log("after Each");
+  });
+});
