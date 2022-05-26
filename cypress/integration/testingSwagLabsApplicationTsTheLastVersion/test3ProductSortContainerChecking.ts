1c47307d927328839690413773edaaa99483cfff
MariiaYaskiv <lytvyn.mariia@gmail.com>
2022-05-26 03:53:15 +0300

    added new tests to test3ProductSortContainerChecking  for Swablabs Application

--- cypress/integration/testingSwagLabsApplicationTsTheLastVersion/test3ProductSortContainerChecking.ts
+++ cypress/integration/testingSwagLabsApplicationTsTheLastVersion/test3ProductSortContainerChecking.ts
@@ -1,68 +1,81 @@
-import { ContainerPageSwaglabs } from "../../support/pages/swaglabsPagesTheLastVersion/containerPageSwaglabs"
-import usersForSwaglabs from "../../data/urersForSwaglabs";
+import { ContainerPageSwaglabs } from "../../support/pages/swaglabsPagesTheLastVersion/containerProductPageSwaglabs"
+import usersForSwaglabs from "../../data/usersForSwaglabs";
 import { LoginPageSwaglabs } from "../../support/pages/swaglabsPagesTheLastVersion/loginPageSwaglabs";
 import { ProductPageSwaglabs } from "../../support/pages/swaglabsPagesTheLastVersion/productPageSwaglabs";
+
 
 
 const l = new LoginPageSwaglabs();
 const p = new ProductPageSwaglabs();
 const ct = new ContainerPageSwaglabs();
 
+
 describe ("Test suite 3", function () {
 
-    beforeEach(function () {
-        l.open()
-        .loginToTheApp(usersForSwaglabs.standardUser)
-        p.puroductLabelChecking()
-    });
+            beforeEach(function () {
+                l.open()
+                .loginToTheApp(usersForSwaglabs.standardUser)
+                p.productLabelChecking()
+            });
 
-it('Test 1 "Select "Price (high to low)" (dropdown)" ', function () {
-
+it('Test 1 "Select "Price (high to low)" and chack it praces (dropdown)" ', function () {
+        
             ct.filterByText("Price (high to low)")
             .waitForSeconds(3)
-            .filterByIndex(1);
+            .filterByIndexFromHighToLowCheckOnlyPrice()
     });
 
-    it('Test 2 "Select "Price (Name (A to Z))" (dropdown)" ', function () {
-
+    it('Test 2 "Select "Price (Name (A to Z))" and chack it praces (dropdown)" ', function () {
+       
             ct.filterByText("Name (A to Z)")
             .waitForSeconds(3)
-            .filterByIndex(1);
+            .filterByIndexFromAtoZCheckOnlyPrice()
     });
 
-    it('Test 3 "Select "Price (Name (Z to A))" (dropdown)" ', function () {
-
+    it('Test 3 "Select "Price (Name (Z to A))" and chack it praces (dropdown)" ', function () {
+     
             ct.filterByText("Name (Z to A)")
             .waitForSeconds(3)
-            .filterByIndex(1);
+            .filterByIndexFromZtoACheckOnlyPrice()
+           
     });
 
-    
-    it('Test 4 "Select "Price (low to high)" (dropdown)" ', function () {
-
+    it('Test 4 "Select "Price (low to high)" and chack it praces (dropdown)" ', function () {
+        
             ct.filterByText("Price (low to high)")
             .waitForSeconds(3)
-            .filterByIndex(1);
+            .filterByIndexFromLowToHighCheckOnlyPrice()
     });
 
-    it('Test 5 "Select items of conteiner one by one (dropdown)" ', function () {
+    it.only('Test 5 "Select items of container one by one (dropdown)" ', function () {
+        
             ct.filterByText("Price (high to low)")
             .waitForSeconds(3)
-            .filterByIndex(1)
             .filterByText("Name (A to Z)")
             .waitForSeconds(3)
-            .filterByIndex(1)
             .filterByText("Name (Z to A)")
             .waitForSeconds(3)
-            .filterByIndex(1)
             .filterByText("Price (low to high)")
             .waitForSeconds(3)
-            .filterByIndex(1)
 
     });
 
-    it.only('Test 6 "Product conteiner should contain four items" ', function () {
+    it.skip('Test 6 "Check if the product container works properly (places the filter well)" ', function () {
         ct.productSortContainerCheckingQuantity()
+        .filterByText("Name (A to Z)")
+        .filterByIndexFromAtoZ()
+        .filterByIndexFromAtoZCheckOnlyPrice()
+        ct.filterByText("Name (Z to A)")
+        .filterByIndexFromZtoACheckOnlyPrice()
+        ct.filterByIndexFromZtoA()
+        .filterByText("Price (high to low)")
+        .filterByIndexFromHighToLowCheckOnlyPrice()
+        ct.filterByIndexFromHighToLow()
+        .filterByText("Price (low to high)")
+        .filterByIndexFromLowToHighCheckOnlyPrice()
+        ct.filterByIndexFromLowToHigh()
+        //.itemPricesList()
+       
 
 });
 
@@ -71,4 +84,3 @@ it('Test 1 "Select "Price (high to low)" (dropdown)" ', function () {
     });
 
 })
-
