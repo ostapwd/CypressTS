62525ea6435bbc386717e903634e61010e2af127
VitalikTR <vitaliktr@gmail.com>
2022-05-26 18:52:48 +0300

    added test api

---
+++ cypress/integration/ParaBank/API/testAPI.ts
@@ -0,0 +1,44 @@
+
+
+describe ("Second test suite for Parabank", () => {
+    it("GET request", () => {
+        let cookie
+
+        cy.request({
+            method: "GET",
+            url: "https://parabank.parasoft.com/parabank/register.htm"
+        }).then((response) => {
+            cy.log(JSON.stringify(response, null, 2));
+            expect(response.status).to.be.equal(200);
+            cy.getCookie('session_id').then((c) => {
+                cookie = c
+            })
+        });
+
+        cy.request({
+            method: "POST",
+            url: "https://parabank.parasoft.com/parabank/register.htm",
+            form: true,
+            // headers: {
+            //     'my-token-x': cookie.value,
+            //   },
+            body:{
+                "customer.firstName": "re",
+                "customer.lastName": "re",
+                "customer.address.street": "re",
+                "customer.address.city": "re",
+                "customer.address.state": "re",
+                "customer.address.zipCode": "re",
+                "customer.phoneNumber": "re",
+                "customer.ssn": "re",
+                "customer.username": "re",
+                "customer.password": "re",
+                "repeatedPassword": "re"
+            }
+        }).then((response) => {
+            cy.log(JSON.stringify(response, null, 2));
+        });
+    })
+   
+ })
+
