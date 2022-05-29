7c0fa9cfcd770af583e8b5f5a61f05f72d9d33da
Natalia_Paraniak <92261072+NataParaniak@users.noreply.github.com>
2022-05-30 00:35:12 +0300

    frame1

--- cypress/integration/successfully bought the product.js
+++ cypress/integration/successfully bought the product.js
@@ -1,25 +1,25 @@
 import loginPage from "../support/pages/loginPage";
 import registrationPage from "../support/pages/registrationPage";
-
+import users from "../data/users";
 
 
 describe(" Log in", function(){
     it("google", function(){
         loginPage.open()
-        loginPage.loginUser('standard_user','secret_sauce')
-        cy.get('#add-to-cart-sauce-labs-backpack').click()
-        cy.wait(1000)
-        cy.get('.shopping_cart_link').click()
-        cy.get('#checkout').click()
+        loginPage.loginUser(users.standardUser)
+        loginPage.addToCard.click() ;       
+        loginPage.shoppingCard.click()
+        loginPage.checkOut.click()         
         registrationPage.registrationUser('Paraniak','Nataliia','79491')
-        cy.wait(1000)
-        cy.get('#continue').click()
-        cy.wait(1000)
-        cy.get('#finish').click()
+        registrationPage.continueEnd.click()
+        registrationPage.finishEnd.click()
+        //cy.get('#continue').click()
+        //cy.get('#finish').click()
     });
     it("choose the two cheapest products", function(){
         loginPage.open()
-        loginPage.loginUser('standard_user','secret_sauce')
+        loginPage.loginUser(users.standardUser)
+
         cy.get('.product_sort_container').select('Price (low to high)')
         cy.get('#add-to-cart-sauce-labs-onesie').click()
         cy.get('.shopping_cart_link').click()
--- cypress/support/pages/loginPage.js
+++ cypress/support/pages/loginPage.js
@@ -3,10 +3,16 @@ class LoginPage{
     get userNameInput(){return cy.get('#user-name')}
     get passwordInput(){return cy.get('#password')}
     get buttonCkick(){return cy.get('#login-button')}
- loginUser(username,password){
-     this.userNameInput.type(username)
-     this.passwordInput.type(password)
+    get addToCard(){return cy.get('#add-to-cart-sauce-labs-backpack')}
+    get shoppingCard(){return cy.get('.shopping_cart_link')}
+    get checkOut(){return cy.get('#checkout')}
+    
+    
+ loginUser(user){
+     this.userNameInput.type(user.username)
+     this.passwordInput.type(user.password)
      this.buttonCkick.click()
+     return this
 }
 open(){
     cy.visit('https://www.saucedemo.com/');
