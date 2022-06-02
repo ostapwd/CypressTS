e0dcf8599f904cc97296a8d899dcac1b69f9a4db
Dimaua <dimarugby@gmail.com>
2022-06-02 15:12:41 +0300

    Mocha intro

--- cypress/integration/test1.js
+++ cypress/integration/test1.js
@@ -1,7 +1,7 @@
 describe('Test suite', function () {
   it('Test1', function () {
     cy.visit('https://www.saucedemo.com/');
-    cy.wait(2000);
+    //cy.wait(2000);
 
     //cy.get('form > div').click();
 
@@ -19,5 +19,58 @@ describe('Test suite', function () {
     cy.get("[id*='add-to-cart']").each((item) => item.click());
 
     cy.get('#react-burger-menu-btn').click();
+  });
+});
+
+describe('Test suite for login', function () {
+  before(function () {
+    cy.log('Main before');
+  });
+
+  describe('Test suite for login (positive)', function () {
+    it('Test1 (positive login)', function () {
+      cy.visit('https://www.saucedemo.com/');
+
+      cy.get('#user-name').type('standard_user');
+
+      cy.get('#password').type('secret_sauce');
+
+      cy.get('#login-button').click();
+
+      cy.get('span.title').should('contain', 'Products');
+    });
+
+    it('Test2 positive login', function () {
+      cy.visit('https://www.saucedemo.com/');
+
+      cy.get('#user-name').type('standard_user');
+
+      cy.get('#password').type('secret_sauce');
+
+      cy.get('#login-button').click();
+
+      cy.get('span.title').should('contain', 'Products');
+    });
+  });
+
+  describe('Test suite for login (negative)', function () {
+    it('Test 3 (negative login)', function () {
+      cy.visit('https://www.saucedemo.com/');
+
+      cy.get('#user-name').type('standard_user');
+
+      cy.get('#password').type('secret_sauce111');
+
+      cy.get('#login-button').click();
+
+      cy.get("h3[data-test = 'error']").should(
+        'contain',
+        'Epic sadface: Username and password do not match any user in this service'
+      );
+    });
+  });
+
+  after(function () {
+    cy.log('Main after');
   });
 });
