c46e11a2443de0a4a8fdcba457f5f81a1f89da3a
Svitlana Krokhmalna <sveta.fetsyk@gmail.com>
2022-06-02 18:56:49 +0300

    Steps

--- cypress/integration/CustomerRegistration.feature
+++ cypress/integration/CustomerRegistration.feature
@@ -5,3 +5,17 @@ Feature: Customer registration form
     Given A customer navigates to the registration page
     When They provide correct registration details
     Then New customer is successfully registered
+
+  @smoke
+  Scenario: A customer can register himself on the site
+    Given A customer navigates to the registration page
+    When They provide incorrect registration details
+    Then Customer recieve error message
+
+  @smoke
+  Scenario: A customer cannot register himself on the site twice
+    Given A customer navigates to the registration page
+    When They provide correct registration details
+    And They go to the registration page again
+    And They provide correct registration details
+    Then Customer recieve error message that such username already exists
--- cypress/stepDefinitions/MySteps.ts
+++ cypress/stepDefinitions/MySteps.ts
@@ -9,17 +9,53 @@ import {BillPage} from "../support/pages/billPage";
 import {ContactsPage} from "../support/pages/contactsPage";
 
 let registerPage;
+let customerPage;
 
+// Case #1: successful registration
 Given(/^A customer navigates to the registration page$/, function () {
     registerPage = new RegisterPage().open();
 });
 
 When(/^They provide correct registration details$/, function () {
-    registerPage.registerCustomer(customers.validCustomer).waitForPageToBeLoaded();
+    customerPage = registerPage.registerCustomer(customers.validCustomer_1).waitForPageToBeLoaded();
 });
 
 Then(/^New customer is successfully registered$/, function () {
-    registerPage.registerCustomer(customers.validCustomer).waitForPageToBeLoaded().welcomeHeader().then(header => {
-        expect(header.text()).to.be.equal(`Welcome ${customers.validCustomer.username}`);
+    customerPage.welcomeHeader().then(header => {
+        expect(header.text()).to.be.equal(`Welcome ${customers.validCustomer_1.username}`);
+    });
+});
+
+// Case #2: form validation error
+Given(/^A customer navigates to the registration page$/, function () {
+    registerPage = new RegisterPage().open();
+});
+
+When(/^They provide incorrect registration details$/, function () {
+    registerPage.registerCustomer(customers.invalidCustomer).waitForPageToBeLoaded();
+});
+
+Then(/^Customer recieve error message$/, function () {
+    cy.get("#customer\\.lastName\\.errors").then(span => {
+        expect(span.text()).to.be.equal("Last name is required.");
+    });
+});
+
+// Case #3: registration of existing user
+Given(/^A customer navigates to the registration page$/, function () {
+    registerPage = new RegisterPage().open();
+});
+
+When(/^They provide correct registration details$/, function () {
+    registerPage.registerCustomer(customers.validCustomer_1).waitForPageToBeLoaded();
+});
+
+When(/^They go to the registration page again$/, function () {
+    registerPage.open();
+});
+
+Then(/^Customer recieve error message that such username already exists$/, function () {
+    cy.get("#customer\\.username\\.errors").then(span => {
+        expect(span.text()).to.be.equal("This username already exists.");
     });
 });
