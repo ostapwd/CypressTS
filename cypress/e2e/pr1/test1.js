653ab61a1d22b14e6ae2b606952213f6b0d945a0
Dimaua <dimarugby@gmail.com>
2022-11-13 01:53:28 +0200

    cypress.json file removed

--- cypress/e2e/pr1/test1.js
+++ cypress/e2e/pr1/test1.js
@@ -32,3 +32,34 @@ describe('Test suit', () => {
         cy.xpath("//*[@id='react-burger-menu-btn']").click();
     })
 });
+
+describe('Test suite for login',()=>{
+    describe('Test suite for login (positive)',()=>{
+    it('Test1 (positive login)', ()=>{
+        cy.visit("https://www.saucedemo.com/")
+        cy.get("#user-name").type('standard_user');
+        cy.get("#password").type('secret_sauce');
+        cy.get("#login-button").click();
+        cy.get("span.title").should('have.text', 'Products')
+        
+    })
+    it('Test2 (positive login)', ()=>{
+        cy.visit("https://www.saucedemo.com/")
+        cy.get("#user-name").type('standard_user');
+        cy.get("#password").type('secret_sauce');
+        cy.get("#login-button").click();
+        cy.get("span.title").should('contain', 'Products')
+        
+    })
+})
+    describe('Test suite for login (negative)',()=>{
+        it('Test3 (negative login)', ()=>{
+            cy.visit("https://www.saucedemo.com/")
+            cy.get("#user-name").type('standard_user');
+            cy.get("#password").type('secret_sauce1');
+            cy.get("#login-button").click();
+            cy.get("h3[data-test='error']").should('contain', 'Epic sadface: Username and password do not match any user in this service')
+            
+        })
+    })
+})
