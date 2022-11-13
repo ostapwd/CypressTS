693b686f4cb9922704b9a4ec2d234c20220d56cf
Dimaua <dimarugby@gmail.com>
2022-11-14 00:46:24 +0200

    mochaIntro

--- cypress/e2e/pr1/test1.js
+++ cypress/e2e/pr1/test1.js
@@ -33,10 +33,29 @@ describe('Test suit', () => {
     })
 });
 
-describe('Test suite for login',()=>{
-    describe('Test suite for login (positive)',()=>{
-    it('Test1 (positive login)', ()=>{
+describe.only('Test suite for login',()=>{
+
+    before(() => {
+        cy.log("Main before")
+    });
+
+    beforeEach(() => {
+        cy.log("MAIN beforeEach")
         cy.visit("https://www.saucedemo.com/")
+       
+    });
+    describe('Test suite for login (positive)',()=>{
+
+        before(() => {
+            cy.log("Internal before")
+        });
+
+        beforeEach(() => {
+            cy.log("beforeEach")
+            
+        });
+    it('Test1 (positive login)', ()=>{
+       
         cy.get("#user-name").type('standard_user');
         cy.get("#password").type('secret_sauce');
         cy.get("#login-button").click();
@@ -44,22 +63,46 @@ describe('Test suite for login',()=>{
         
     })
     it('Test2 (positive login)', ()=>{
-        cy.visit("https://www.saucedemo.com/")
+        
         cy.get("#user-name").type('standard_user');
         cy.get("#password").type('secret_sauce');
-        cy.get("#login-button").click();
+        cy.get("#login-button").click(); 
         cy.get("span.title").should('contain', 'Products')
         
     })
+afterEach(() => {
+    cy.log("afterEach")
+});
+
+    after(() => {
+        cy.log("Internal after")
+    });
 })
     describe('Test suite for login (negative)',()=>{
-        it('Test3 (negative login)', ()=>{
-            cy.visit("https://www.saucedemo.com/")
+
+        beforeEach(() => {
+            cy.log("beforeEach")
+            
             cy.get("#user-name").type('standard_user');
+        });
+
+        it('Test3 (negative login)', ()=>{
+            
+            
             cy.get("#password").type('secret_sauce1');
             cy.get("#login-button").click();
             cy.get("h3[data-test='error']").should('contain', 'Epic sadface: Username and password do not match any user in this service')
             
         })
+
+        it('Test4 (negative login)', ()=>{
+                        
+            cy.get("#login-button").click();
+            cy.get("h3[data-test='error']").should('contain', 'Epic sadface: Password is required')
+            
+        })
     })
+    after(() => {
+        cy.log("Main after")
+    });
 })
